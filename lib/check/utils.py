e940945f75271f8980fe70fe17f787c8504dc229
Koos85 <koosjoosten@gmail.com>
2026-05-06 15:20:47 +0200

    fix

--- lib/check/utils.py
+++ lib/check/utils.py
@@ -4,13 +4,13 @@ def uint(val: int | None) -> int | None:
     return val
 
 
-def usec_to_seconds(val: int | None):
-    if not isinstance(val, int):
+def usec_to_seconds(val: float | None) -> float | None:
+    if not isinstance(val, (float, int)):
         return
     return val / 1_000_000
 
 
-def usec_to_seconds_as_int(val: int | None):
-    if not isinstance(val, int):
+def usec_to_seconds_as_int(val: float | None) -> int | None:
+    if not isinstance(val, (float, int)):
         return
     return int(val / 1_000_000)

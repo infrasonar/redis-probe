af74cc64d49ae4aea9d7642f1cab85e46cbce638
Jeroen van der Heijden <jeroen@cesbit.com>
2026-05-06 15:29:05 +0200

    .

--- lib/check/utils.py
+++ lib/check/utils.py
@@ -4,23 +4,13 @@ def uint(val: int | None) -> int | None:
     return val
 
 
-<<<<<<< HEAD
-def usec_to_seconds(val: int | None) -> float | None:
-    if not isinstance(val, int):
-=======
 def usec_to_seconds(val: float | None) -> float | None:
     if not isinstance(val, (float, int)):
->>>>>>> 139e092b875685c1126e0e9629a51daa6896f327
         return
     return val / 1_000_000
 
 
-<<<<<<< HEAD
-def usec_to_seconds_as_int(val: int | None) -> int | None:
-    if not isinstance(val, int):
-=======
 def usec_to_seconds_as_int(val: float | None) -> int | None:
     if not isinstance(val, (float, int)):
->>>>>>> 139e092b875685c1126e0e9629a51daa6896f327
         return
     return int(val / 1_000_000)
